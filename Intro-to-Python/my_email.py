96d94d2cbbc9b46da3d29c30a800c286fd06e659
Ray Wonkwon Lee <dload1392@gmail.com>
2021-03-16 14:59:29 +0900

    Update function-and-class notebook
    Update jupyter notebbok
    Learn from-import

---
+++ Intro-to-Python/my_email.py
@@ -0,0 +1,12 @@
+# my_email.py
+
+class Email:
+    sender = ""
+    
+    def send_mail(self, recv, subject, contents):
+        print("From:\t" + self.sender)
+        print("To:\t" + recv)
+        print("Subject:\t" + subject)
+        print("Contents")
+        print(contents)
+        print("-"*20)           # Python에서는 String값도 곱해서 출력이 가능
