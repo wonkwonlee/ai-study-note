05eeffb2b5d4de7f8ccf98f3bc29137bb0161cbc
Ray Wonkwon Lee <dload1392@gmail.com>
2021-03-16 23:48:54 +0900

    Create Fibonacci_Card.py Recursive method and list method

--- Intro-to-Python/Fibonacci_Card.py
+++ Intro-to-Python/Fibonacci_Card.py
@@ -1,11 +1,28 @@
-# def fibonacci(n):
-#     F_0 = 0  # Initial term = 0
-#     F_1 = 1  # F_1 = 1
-#     F_n = 0
-#
-# fibonacci(5)
+"""
+피보나치 수열을 출력하는 프로그램
 
+조건 1. 함수를 활용하여 문제에서 언급한 피보나치 알고리즘을 작성하시오.
+조건 2. for 반복문과 range() 함수를 통해 출력을 제어하시오. (최소 8항의 값 21까지 출력 )
+"""
 
-F_0 = 0  # Initial term = 0
-F_1 = 1  # F_1 = 1
-F_n = 0
+# Recursive method
+def fibonacci(n):
+    if n <= 1:
+        return n
+    else:
+        return(fibonacci(n-1) + fibonacci(n-2))
+
+print("========재귀문 방식========")
+for i in range(8):
+    print(fibonacci(i+1))
+
+# List method
+fib_list = [1,1]
+
+print("========배열 방식========")
+for i in range(0,10):
+    # 파이썬에서 음수 인덱스는 가장 뒤부터 시작한다
+    # 즉, arr[-1]은 배열 맨 뒤, arr[-2]은 맨 뒤에서 2번째
+    fib_list.append(fib_list[-1] + fib_list[-2])
+
+print(fib_list)
