e107c958962c1fa2ff39de1f2c0280b3b5d2ddd4
Ray Wonkwon Lee <dload1392@gmail.com>
2021-03-24 16:56:22 +0900

    Update mazeBFS.py

--- Data-Structure-and-Algorithm/mazeBFS.py
+++ Data-Structure-and-Algorithm/mazeBFS.py
@@ -1,1 +1,37 @@
-dd
+'''
+Test Case
+5 6 
+101010
+111111
+000001
+111111
+111111
+'''
+def dfs(x, y):
+    # Validation check
+    if x <= -1 or x >= n or y <= -1 or y >= m:
+        return False
+
+
+
+    return False
+
+# Length = n, Width = m
+n, m = map(int, input().split())
+graph = []
+
+# Fill graph with values
+for i in range(n):
+    graph.append(list(map(int, input())))
+
+# # print(graph); [[0, 0, 1, 1, 0], [0, 0, 0, 1, 1], [1, 1, 1, 1, 1], [0, 0, 0, 0, 0]]
+
+# result = 0
+# for i in range(n):
+#     for j in range(m):
+#         if dfs(i, j):   # if dfs(i, j) is True
+#             result += 1
+
+# print(result)
+
+print(graph)
