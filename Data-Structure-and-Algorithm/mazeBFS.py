ca9f6951ecc5a9c3a1020f01433d3b5be3332193
Ray Wonkwon Lee <dload1392@gmail.com>
2021-03-24 20:42:26 +0900

    Update mazeBFS.py
    Solve mazeBFS.py.

--- Data-Structure-and-Algorithm/mazeBFS.py
+++ Data-Structure-and-Algorithm/mazeBFS.py
@@ -6,32 +6,53 @@ Test Case
 000001
 111111
 111111
+
+>> 10
+
+deque : double-endedd queue, which can handle data in both ends.
 '''
-def dfs(x, y):
-    # Validation check
-    if x <= -1 or x >= n or y <= -1 or y >= m:
-        return False
+from collections import deque
 
-
-
-    return False
-
-# Length = n, Width = m
-n, m = map(int, input().split())
+# Length = N, Width = M
+N, M = map(int, input().split())
+# Map info in 2-D list
 graph = []
 
+# move directions
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
 # Fill graph with values
-for i in range(n):
+for i in range(N):
     graph.append(list(map(int, input())))
 
-# # print(graph); [[0, 0, 1, 1, 0], [0, 0, 0, 1, 1], [1, 1, 1, 1, 1], [0, 0, 0, 0, 0]]
+# BFS method
+def bfs(x, y):
+    queue = deque()
+    queue.append((x, y))
 
-# result = 0
-# for i in range(n):
-#     for j in range(m):
-#         if dfs(i, j):   # if dfs(i, j) is True
-#             result += 1
+    # While queue is not empty
+    while queue:
+        x, y = queue.popleft()
 
-# print(result)
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
 
-print(graph)
+            # Ignore out of bound
+            if nx < 0 or nx >= N or ny < 0 or ny >= M:
+                continue
+            # Ignore if current node is corner
+            if graph[nx][ny] == 0:
+                continue
+            # Store the shortest path only if it is the first vist
+            if graph[nx][ny] == 1:
+                # print((x,y), graph[x][y])
+                graph[nx][ny] = graph[x][y] + 1
+                queue.append((nx, ny))
+
+    # Print graph with each cost
+    # print(graph)  
+    return graph[N-1][M-1]
+    
+print(bfs(0, 0))
