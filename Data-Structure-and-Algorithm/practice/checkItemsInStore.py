1cb26dc5e86a78ae5cf16fd17015a17e29aec535
wonkwonlee <dload1392@gmail.com>
2021-04-01 21:24:29 +0900

    Solve Algorithm practice problems
    Solve checkItemsInStore.py and riceCakeCutter.py.

--- Data-Structure-and-Algorithm/practice/checkItemsInStore.py
+++ Data-Structure-and-Algorithm/practice/checkItemsInStore.py
@@ -1,7 +1,40 @@
+"""
+Check Items In Store
 
+Binary Search
+"""
+
+
+def find_items(target, array, start, end):
+    # If start > end, that means all elements are visited
+    if start > end:
+        return None
+    mid = (start + end) // 2
+    if array[mid] == target:
+        return mid
+    elif array[mid] > target:
+        return find_items(target, array, start, mid - 1)
+    else:
+        return find_items(target, array, mid + 1, end)
+
+
+# N is number of items at the store
 n = int(input())
-store, request = [], []
 
-for i in range(n):
-    input_data = int(input().split())
-    store.append(input_data)
+# Store data into store_items list
+store_items = list(map(int, input().split()))
+
+# M is the number of items customer request
+m = int(input())
+
+# Store data into request_items list
+request_items = list(map(int, input().split()))
+
+# Sort store_items list
+store_items.sort()
+for x in request_items:
+    result = find_items(x, store_items, 0, n - 1)
+    if result is None:
+        print("No")
+    else:
+        print("Yes")
--- Data-Structure-and-Algorithm/practice/riceCakeCutter.py
+++ Data-Structure-and-Algorithm/practice/riceCakeCutter.py
@@ -1,22 +1,31 @@
+def height_search(array, nums, target, start, end):
+    # Binary search middle index
+    height = (start + end) // 2
+    # If there is no optimal point, return current height value
+    if start > end:
+        return height
+    # Temporary var
+    curr_height = 0
+    # Sum of current rice cake
+    for x in array:
+        curr_height += max(x - nums[height], 0)
+    # Binary search
+    if curr_height == target:
+        return height
+    elif curr_height > target:
+        return height_search(array, nums, target, height + 1, end)
+    else:
+        return height_search(array, nums, target, start, height - 1)
+
+
+# N is number of rice cakes, M is the requested height of rice cake
 n, m = map(int, input().split())
+# Store the height of each rice cake
+rice_cake = list(map(int, input().split()))
 
-nums = []
-for i in range(n):
-    input_data = int(input().split())
-    nums.append(input_data)
+# Initialize a list to store height from 0 to max value
+heights = [x for x in range(max(rice_cake) + 1)]
+# Find the
+target_height = height_search(rice_cake, heights, m, min(heights), max(heights))
 
-mx, mn = max(nums), 0
-result = []
-
-while sum(result) == m:
-    height = (mx + mn) // 2
-    for x in nums:
-        result.append(max(x - height, 0))
-
-    if sum(result) > m:
-        mn = height + 1
-
-    elif sum(result) < m: 
-        mx -= 1
-
-print(height)
+print(target_height)
