18c55dc36aa13edcdfc66695cfc3cf0850ed03a3
wonkwonlee <dload1392@gmail.com>
2021-04-02 13:33:53 +0900

    Update checkItemsInStore.py and riceCakeCutter.py
    Update checkItemsInStore.py and riceCakeCutter.py with example codes.
    For checkItemsInStore.py, add counting sort algorithm.

--- Data-Structure-and-Algorithm/practice/checkItemsInStore.py
+++ Data-Structure-and-Algorithm/practice/checkItemsInStore.py
@@ -5,7 +5,7 @@ Binary Search
 """
 
 
-def find_items(target, array, start, end):
+def binary_search(target, array, start, end):
     # If start > end, that means all elements are visited
     if start > end:
         return None
@@ -13,9 +13,9 @@ def find_items(target, array, start, end):
     if array[mid] == target:
         return mid
     elif array[mid] > target:
-        return find_items(target, array, start, mid - 1)
+        return binary_search(target, array, start, mid - 1)
     else:
-        return find_items(target, array, mid + 1, end)
+        return binary_search(target, array, mid + 1, end)
 
 
 # N is number of items at the store
@@ -33,8 +33,56 @@ request_items = list(map(int, input().split()))
 # Sort store_items list
 store_items.sort()
 for x in request_items:
-    result = find_items(x, store_items, 0, n - 1)
+    result = binary_search(x, store_items, 0, n - 1)
     if result is None:
         print("No")
     else:
         print("Yes")
+
+
+# # Example 1: Binary Search
+# def binary_search(array, target, start, end):
+#     # Use while instead of if start > end
+#     while start <= end:
+#         mid = (start+end) // 2
+#         if array[mid] == target:
+#             return mid
+#         elif array[mid] > target:
+#               end = mid - 1
+#         else:
+#           start = mid + 1
+#     return None
+#
+# n = int(input())
+#
+# array = list(map(int, input().split()))
+# array.sort()
+#
+# m = int(input())
+# x = list(map(int, input().split()))
+#
+# for i in x:
+#   result = binary_search(array, i, 0, n - 1)
+#   if result != None:
+#     print('yes', end = ' ')
+#   else:
+#     print('no', end = ' ')
+
+
+# # Example 2: Counting Sort
+# n = int(input())
+# # Create an empty array filled with out of boundary value
+# array = [0] * 1000001
+#
+# # Mark items to array as 1
+# for i in input().split():
+#     array[int(i)] = 1
+#
+# m = int(input())
+# x = list(map(int, input().split()))
+#
+# for i in x:
+#     if array[i] == 1:
+#         print('yes', end = ' ')
+#     else:
+#         print('no', end = ' ')
--- Data-Structure-and-Algorithm/practice/riceCakeCutter.py
+++ Data-Structure-and-Algorithm/practice/riceCakeCutter.py
@@ -1,21 +1,21 @@
 def height_search(array, nums, target, start, end):
     # Binary search middle index
-    height = (start + end) // 2
-    # If there is no optimal point, return current height value
+    mid = (start + end) // 2
+    # If there is no optimal point, return current rice cake height
     if start > end:
-        return height
+        return mid
     # Temporary var
     curr_height = 0
     # Sum of current rice cake
     for x in array:
-        curr_height += max(x - nums[height], 0)
+        curr_height += max(x - nums[mid], 0)
     # Binary search
     if curr_height == target:
-        return height
+        return mid
     elif curr_height > target:
-        return height_search(array, nums, target, height + 1, end)
+        return height_search(array, nums, target, mid + 1, end)
     else:
-        return height_search(array, nums, target, start, height - 1)
+        return height_search(array, nums, target, start, mid - 1)
 
 
 # N is number of rice cakes, M is the requested height of rice cake
@@ -29,3 +29,29 @@ heights = [x for x in range(max(rice_cake) + 1)]
 target_height = height_search(rice_cake, heights, m, min(heights), max(heights))
 
 print(target_height)
+
+
+# # Example 1: Binary Search
+# n, m = list(map(int, input().split(' ')))
+# array = list(map(int, input().split()))
+#
+# start = 0
+# end = max(array)
+# result = 0
+#
+# # Binary search until start > end
+# while start <= end:
+#     # Sum of customer's rice cakes
+#     total = 0
+#     mid = (start + end) // 2
+#     for x in array:
+#         if x > mid:
+#             total += x - mid
+#     # Binary search algorithm
+#     if total < m:
+#         end = mid - 1
+#     else:
+#         result = mid
+#         start = mid + 1
+#
+# print(result)
