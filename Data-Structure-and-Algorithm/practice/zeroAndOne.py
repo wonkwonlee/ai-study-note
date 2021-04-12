953972a2fa0b085947c240a966cd53775c81f681
wonkwonlee <dload1392@gmail.com>
2021-04-12 17:00:34 +0900

    Solve zeroAndOne.py
    Solve practice problem zeroAndOne.py.

--- Data-Structure-and-Algorithm/practice/zeroAndOne.py
+++ Data-Structure-and-Algorithm/practice/zeroAndOne.py
@@ -9,12 +9,25 @@ Input: 01010
 Output: 2
 """
 
-s = str(input())
 
-count = 0
-result = 0
+# String input
+s = input()
+# count0 is 0s to 1s, and count1 is 1s to 0s
+count0, count1 = 0, 0
 
-# From 0 to 1
-for i in range(len(s)):
-    if s[i] == 0:
-        
+# Check the first element in the string
+if s[0] == '0':
+    count0 += 1
+else:
+    count1 += 1
+
+for i in range(len(s) - 1):
+    if s[i] != s[i + 1]:
+        # If next element is 0, increase count0
+        if s[i + 1] == '0':
+            count0 += 1
+        # If next element is 1, increase count1
+        else:
+            count1 += 1
+
+print(min(count0, count1))
