70bd7a36d5dd14089ac6025dcdd9df975193d6d3
wonkwonlee <dload1392@gmail.com>
2021-04-20 10:25:24 +0900

    Add comments

--- Data-Structure-and-Algorithm/practice/binarySearch.py
+++ Data-Structure-and-Algorithm/practice/binarySearch.py
@@ -4,15 +4,16 @@ Binary Search
 Use binary search to find target element in the array.
 """
 
+# N number of elements and the target value
 n, target = map(int, input().split())
-
 array = list(map(int, input().split()))
-
 
 start = 0
 end = len(array) - 1
 
-def binary_search (start, end, target):
+
+# Binary search using recursion
+def binary_search(start, end, target):
     mid = (start + end) // 2
     if start > end:
         return None
@@ -23,10 +24,10 @@ def binary_search (start, end, target):
     else:
         return binary_search(mid + 1, end, target)
 
+
 result = binary_search(start, end, target)
 
-if result == None:
+if result is None:
     print("The target element does not exist.")
 else:
     print(result + 1)
-
