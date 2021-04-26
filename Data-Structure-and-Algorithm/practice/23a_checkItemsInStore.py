65fe3b992632af9f697a9f2854891e6888d5fcc1
wonkwonlee <dload1392@gmail.com>
2021-04-26 10:27:04 +0900

    Add comments
    Update comments in problem 23 a, b.

--- Data-Structure-and-Algorithm/practice/23a_checkItemsInStore.py
+++ Data-Structure-and-Algorithm/practice/23a_checkItemsInStore.py
@@ -1,6 +1,6 @@
 """
 Check Items in Store
-# Binary Search
+Binary Search using iteration
 
 5
 8 3 7 9 2
@@ -8,35 +8,47 @@ Check Items in Store
 5 7 9
 >> No Yes Yes
 """
+
+# Input data
 n = int(input())
 store = list(map(int, input().split()))
 m = int(input())
 cust = list(map(int, input().split()))
+
+# Test case
 # n = 5
 # store = [8,3,7,9,2]
 # m = 3
 # cust = [5,7,9]
 
+# Store result
 result = []
-
+# Sort items in store
 store.sort()
 
 
+# Binary Search using iteration
 def binary_search(start, end, array, target):
-    mid = (start + end) // 2
-    if start > end:
-        return None
-    if array[mid] == target:
-        return mid
-    elif array[mid] > target:
-        return binary_search(start, mid - 1, array, target)
-    else:
-        return binary_search(mid + 1, end, array, target)
+    # While loop for start <= end
+    while start <= end:
+        mid = (start + end) // 2
+        # If current value is same as the target, return the current index
+        if array[mid] == target:
+            return mid
+        # If current value is larger, decrease the end index
+        elif array[mid] > target:
+            end = mid - 1
+        # If current value is smaller, increase the start index
+        else:
+            start = mid + 1
+    return None
 
 
+# Binary search each item in store
 for item in cust:
     result.append(binary_search(0, n - 1, store, item))
 
+# Print output
 for i in result:
     if i is None:
         print("No", end=' ')
--- Data-Structure-and-Algorithm/practice/23b_checkItemsInStore.py
+++ Data-Structure-and-Algorithm/practice/23b_checkItemsInStore.py
@@ -1,6 +1,6 @@
 """
 Check Items in Store
-# Counting Sort
+Counting Sort
 
 5
 8 3 7 9 2
@@ -8,22 +8,31 @@ Check Items in Store
 5 7 9
 >> No Yes Yes
 """
+
+# Input data
 n = int(input())
 store = list(map(int, input().split()))
 m = int(input())
 cust = list(map(int, input().split()))
+
+# Test case
 # n = 5
 # store = [8,3,7,9,2]
 # m = 3
 # cust = [5,7,9]
 
+# Initialize result array
 result = [0] * 1000001
 
+# Mark existing items in store as 1
 for item in store:
     result[item] = 1
 
+# Check if customer requested items exist in result array
 for i in cust:
+    # If item exists, print yes
     if result[i] == 1:
         print("Yes", end=' ')
+    # If item does not exist, print no
     else:
         print("No", end=' ')
